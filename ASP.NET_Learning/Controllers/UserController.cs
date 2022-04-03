5b1724409673f0a1e38900178203edc033ca8c33
Rodrigo F. (NotBuer) <rodrigosoocer@gmail.com>
2022-04-03 01:07:01 -0300

    User register/login

--- ASP.NET_Learning/Controllers/UserController.cs
+++ ASP.NET_Learning/Controllers/UserController.cs
@@ -16,35 +16,40 @@ namespace ASP.NET_Learning.Controllers
             return View("Index");
         }
 
+        [HttpPost]
         public ActionResult Login()
         {
             return View("Login");
         }
 
+        [HttpPost]
         public ActionResult Register()
         {
             return View("Register");
         }
-        
+
+        [HttpPost]
         public ActionResult LoginProcess(UserModel user)
         {
             SecurityDAO securityDAO = new SecurityDAO();
             UserModel userModel = securityDAO.LoginUser(user);
-
+            
             // First check if the returned user is valid or not.
             if (UserModel.IsUserValid(userModel))
             {
                 // If it is, then redirect the user to the UserHome page.
+                //Session["Username"] = user.Name;
+                Session.Add("Username", user.Name);
                 return View("UserHome", user);
             }
             else
             {
-                return View("Login");
                 // Otherwise, redirect again the user to the Login page.
+                return View("Login");
             }
-
         }
 
+        [HttpPost]
         public ActionResult RegisterProcess(UserModel user)
         {
             SecurityDAO securityDAO = new SecurityDAO();
@@ -54,6 +59,8 @@ namespace ASP.NET_Learning.Controllers
             if (UserModel.IsUserValid(userModel))
             {
                 // If it is, then redirect the user to the UserHome page.
+                //Session["Username"] = user.Name;
+                Session.Add("Username", user.Name);
                 return View("UserHome", user);
             }
             else
@@ -61,7 +68,12 @@ namespace ASP.NET_Learning.Controllers
                 // Otherwise, redirect again the user to the Register page.
                 return View("Register");
             }
-            
+        }
+
+        [HttpPost]
+        public ActionResult UserHome(UserModel user)
+        {
+            return View("UserHome", user);
         }
 
     }
